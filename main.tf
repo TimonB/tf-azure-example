6cc959814dd6c3b98f8c3322bffac585b282605f
tbirk-bhs <97614949+tbirk-bhs@users.noreply.github.com>
2022-01-20 16:13:48 +0100

    wip

--- main.tf
+++ main.tf
@@ -150,93 +150,93 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     }
 }
 
-## Create public IPs
-#resource "azurerm_public_ip" "ghespublicip" {
-#    name                         = "ghesPublicIP"
-#    location                     = "germanywestcentral"
-#    resource_group_name          = azurerm_resource_group.myterraformgroup.name
-#    allocation_method            = "Dynamic"
-#
-#    tags = {
-#        environment = "Terraform Demo"
-#    }
-#}
-#
-#
-#resource "azurerm_network_interface" "ghesnic" {
-#    name                      = "ghesnic"
-#    location                  = "germanywestcentral"
-#    resource_group_name       = azurerm_resource_group.myterraformgroup.name
-#
-#    ip_configuration {
-#        name                          = "myNicConfiguration"
-#        subnet_id                     = azurerm_subnet.myterraformsubnet.id
-#        private_ip_address_allocation = "Dynamic"
-#        public_ip_address_id          = azurerm_public_ip.ghespublicip.id
-#    }
-#
-#    tags = {
-#        environment = "Terraform Demo"
-#    }
-#}
-#
-#resource "azurerm_virtual_machine" "main" {
-#  name                  = "ghes-vm"
-#  location              = "germanywestcentral"
-#  resource_group_name   = azurerm_resource_group.myterraformgroup.name
-#  network_interface_ids = [azurerm_network_interface.ghesnic.id]
-#  vm_size               = "Standard_DS11_v2"
-#
-#  storage_image_reference {
-#    publisher = "GitHub"
-#    offer     = "GitHub-Enterprise"
-#    sku       = "GitHub-Enterprise"
-#    version   = var.ghes-version
-#  }
-#
-#  storage_os_disk {
-#    name              = "ghes-os-storage"
-#    caching           = "ReadWrite"
-#    create_option     = "FromImage"
-#    managed_disk_type = "Premium_LRS"
-#    disk_size_gb      = "200"
-#  }
-#
-#  os_profile {
-#    computer_name  = "myghes"
-#    admin_username = "admin"
-#  }
-#
-#  os_profile_linux_config {
-#    disable_password_authentication = true
-#    ssh_keys {
-#      path     = "/home/admin/.ssh/authorized_keys"
-#      key_data = var.ssh_public_key
-#    }
-# }
-#}
-#
-#resource "azurerm_managed_disk" "ghes-data" {
-#  name                 = "ghes-disk1"
-#  location              = "germanywestcentral"
-#  resource_group_name   = azurerm_resource_group.myterraformgroup.name
-#  storage_account_type = "Standard_LRS"
-#  create_option        = "Empty"
-#  disk_size_gb         = 50
-#}
-#
-#resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-#  managed_disk_id    = azurerm_managed_disk.ghes-data.id
-#  virtual_machine_id = azurerm_virtual_machine.main.id
-#  lun                = "10"
-#  caching            = "ReadWrite"
-#}
-#
-#output "public_ip" {
-#  value       = azurerm_public_ip.ghespublicip.ip_address
-#  description = "The IP address of the GitHub Enterprise Server instance"
-#}
-#
+# Create public IPs
+resource "azurerm_public_ip" "ghespublicip" {
+    name                         = "ghesPublicIP"
+    location                     = "germanywestcentral"
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+resource "azurerm_network_interface" "ghesnic" {
+    name                      = "ghesnic"
+    location                  = "germanywestcentral"
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.ghespublicip.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "ghes-vm"
+  location              = "germanywestcentral"
+  resource_group_name   = azurerm_resource_group.myterraformgroup.name
+  network_interface_ids = [azurerm_network_interface.ghesnic.id]
+  vm_size               = "Standard_DS11_v2"
+
+  storage_image_reference {
+    publisher = "GitHub"
+    offer     = "GitHub-Enterprise"
+    sku       = "GitHub-Enterprise"
+    version   = var.ghes-version
+  }
+
+  storage_os_disk {
+    name              = "ghes-os-storage"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "200"
+  }
+
+  os_profile {
+    computer_name  = "myghes"
+    admin_username = "admin"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/admin/.ssh/authorized_keys"
+      key_data = var.ssh_public_key
+    }
+ }
+}
+
+resource "azurerm_managed_disk" "ghes-data" {
+  name                 = "ghes-disk1"
+  location              = "germanywestcentral"
+  resource_group_name   = azurerm_resource_group.myterraformgroup.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 50
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.ghes-data.id
+  virtual_machine_id = azurerm_virtual_machine.main.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
+
+output "public_ip" {
+  value       = azurerm_public_ip.ghespublicip.ip_address
+  description = "The IP address of the GitHub Enterprise Server instance"
+}
+
 
 #resource "azurerm_kubernetes_cluster" "example" {
 #  name                = "example-aks1"
