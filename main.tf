d0732b2643cadb37ef92f1c61eb91910aae8169c
tbirk-bhs <97614949+tbirk-bhs@users.noreply.github.com>
2022-01-21 08:43:44 +0100

    wip

--- main.tf
+++ main.tf
@@ -247,26 +247,26 @@ output "public_ip" {
 }
 
 
-#resource "azurerm_kubernetes_cluster" "example" {
-#  name                = "example-aks1"
-#  location              = "germanywestcentral"
-#   resource_group_name   = azurerm_resource_group.myterraformgroup.name
-#  dns_prefix          = "exampleaks1"
-#
-#  default_node_pool {
-#    name       = "default"
-#    node_count = 1
-#    vm_size    = "Standard_D2_v2"
-#  }
-#
-#  identity {
-#    type = "SystemAssigned"
-#  }
-#
-#  tags = {
-#    Environment = "Production"
-#  }
-#}
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location              = "germanywestcentral"
+   resource_group_name   = azurerm_resource_group.myterraformgroup.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
 #
 #output "client_certificate" {
 #  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
