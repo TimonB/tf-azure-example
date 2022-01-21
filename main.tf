49ef3fab77001dbec884c1fc3852937fc7511e6b
tbirk-bhs <97614949+tbirk-bhs@users.noreply.github.com>
2022-01-21 08:13:46 +0100

    wip

--- main.tf
+++ main.tf
@@ -188,42 +188,42 @@ resource "azurerm_network_interface" "ghesnic" {
     }
 }
 
-#resource "azurerm_virtual_machine" "ghes-test" {
-#  name                  = "ghes-vm"
-#  location              = "germanywestcentral"
-#  resource_group_name   = azurerm_resource_group.myterraformgroup.name
-#  network_interface_ids = [azurerm_network_interface.ghesnic.id]
-#  vm_size               = "Standard_DS11_v2"
-#
-#  # az vm image list --all -f GitHub-Enterprise
-#  storage_image_reference {
-#    publisher = "GitHub"
-#    offer     = "GitHub-Enterprise"
-#    sku       = "GitHub-Enterprise"
-#    version   = var.ghes-version
-#  }
-#
-#  storage_os_disk {
-#    name              = "ghes-os-storage"
-#    caching           = "ReadWrite"
-#    create_option     = "FromImage"
-#    managed_disk_type = "Premium_LRS"
-#    disk_size_gb      = "200"
-#  }
-#
-#  os_profile {
-#    computer_name  = "myghes"
-#    admin_username = var.username
-#  }
-#
-#  os_profile_linux_config {
-#    disable_password_authentication = true
-#    ssh_keys {
-#      path     = "/home/${var.username}/.ssh/authorized_keys"
-#      key_data = var.ssh_public_key
-#    }
-# }
-#}
+resource "azurerm_virtual_machine" "ghes-test" {
+  name                  = "ghes-vm"
+  location              = "germanywestcentral"
+  resource_group_name   = azurerm_resource_group.myterraformgroup.name
+  network_interface_ids = [azurerm_network_interface.ghesnic.id]
+  vm_size               = "Standard_DS11_v2"
+
+  # az vm image list --all -f GitHub-Enterprise
+  storage_image_reference {
+    publisher = "GitHub"
+    offer     = "GitHub-Enterprise"
+    sku       = "GitHub-Enterprise"
+    version   = var.ghes-version
+  }
+
+  storage_os_disk {
+    name              = "ghes-os-storage"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = "200"
+  }
+
+  os_profile {
+    computer_name  = "myghes"
+    admin_username = var.username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+      path     = "/home/${var.username}/.ssh/authorized_keys"
+      key_data = var.ssh_public_key
+    }
+ }
+}
 
 resource "azurerm_managed_disk" "ghes-data" {
   name                 = "ghes-disk1"
@@ -234,18 +234,18 @@ resource "azurerm_managed_disk" "ghes-data" {
   disk_size_gb         = 50
 }
 
-#resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-#  managed_disk_id    = azurerm_managed_disk.ghes-data.id
-#  virtual_machine_id = azurerm_virtual_machine.ghes-test.id
-#  lun                = "10"
-#  caching            = "ReadWrite"
-#}
-#
-#output "public_ip" {
-#  value       = azurerm_public_ip.ghespublicip.ip_address
-#  description = "The IP address of the GitHub Enterprise Server instance"
-#}
-#
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.ghes-data.id
+  virtual_machine_id = azurerm_virtual_machine.ghes-test.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
+
+output "public_ip" {
+  value       = azurerm_public_ip.ghespublicip.ip_address
+  description = "The IP address of the GitHub Enterprise Server instance"
+}
+
 
 #resource "azurerm_kubernetes_cluster" "example" {
 #  name                = "example-aks1"
