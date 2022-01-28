d3d2aec1d2cdea01b6df2f7d8c4f288677815ac1
tbirk-bhs <97614949+tbirk-bhs@users.noreply.github.com>
2022-01-28 07:46:04 +0100

    wip

--- ghes.tf
+++ ghes.tf
@@ -28,6 +28,31 @@ resource "azurerm_public_ip" "ghespublicip" {
 }
 # ToDo: add Loadbalancer
 
+resource "azurerm_public_ip" "ghes-lb-publicip" {
+  name                = "publicIPForLB"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "ghes-lb" {
+  name                = "loadBalancer"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+  frontend_ip_configuration {
+    name                 = "publicIPAddress"
+    public_ip_address_id = azurerm_public_ip.ghes-lb-publicip.id
+  }
+}
+
+ resource "azurerm_lb_backend_address_pool" "ghes-lb-backend" {
+   loadbalancer_id     = azurerm_lb.ghes-lb.id
+   name                = "BackEndAddressPool"
+ }
+
+
+
 # Add security rules
 resource "azurerm_network_security_group" "ghessecgroup" {
   name                = "ghes-security-group"
